01f5ed4d6276bbce8d87e5afbaa4986cae5391cb
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-11-29 01:35:01 -0800

    Initial commit

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Topbar.css';
+import profile from '../img/profile.png';
 import  { NotificationsNone, Language, Settings } from '@material-ui/icons';
 
 export default function Topbar(){
@@ -25,6 +26,7 @@ export default function Topbar(){
                    <div className="topbarIconContainer">
                       <Settings />
                    </div>
+                   <img src={profile} className="topAvatar" alt="profile"/>
                </div>
            </div>
        </div>
