d1bd83551f16b8dbdf638fd8b2cb946b72af4b18
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-11-29 00:58:43 -0800

    Initial commit

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Topbar.css';
+import  { NotificationsNone } from '@material-ui/icons';
+
+export default function Topbar(){
+   return (
+       <div className="topbar">
+           <div className="topbarWrapper">
+               <div className="topLeft">
+                   <span className="logo">
+                       Yakubuadmin
+                   </span>
+               </div>
+               <div className="topRight">
+                   <div className="topbarIcons">
+                      <NotificationsNone />
+                   </div>
+               </div>
+           </div>
+       </div>
+   );
+}
