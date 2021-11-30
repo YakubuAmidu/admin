74cd9615d66949906faecb098c03c71024a9172a
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-11-30 02:52:33 -0800

    Initial commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Home.css';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+import WidgetSm from '../../components/widgetSm/WidgetSm';
+import WidgetLg from '../../components/widgetLg/WidgetLg';
 import Chart from '../../components/chart/chart';
 import { userData } from '../../dummyData';
 
@@ -9,6 +11,10 @@ export default function Home(){
         <div className="home">
             <FeaturedInfo />
             <Chart data={userData} title="User analytics" grid datakey="Active User"/>
+            <div className="homeWidget">
+                <WidgetSm />
+                <WidgetLg />
+            </div>
         </div>
     )
 }
