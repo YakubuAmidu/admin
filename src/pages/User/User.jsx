106a8a545f15c036a400841bb589de8daf358686
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-11-30 23:20:45 -0800

    Initial commit

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './User.css';
+import UserShow1 from '../../img/UserShowImg1.png';
 
 export default function User(){
     return (
@@ -7,6 +8,22 @@ export default function User(){
             <div className="userTitleContainer">
                 <h1 className="userTitle">Edit</h1>
                 <button className="userAddButton">Create</button>
+            </div>
+
+            <div className="userContainer">
+                <div className="userShow">
+                    <div className="userShowTop">
+                        <img src={UserShow1} alt="" className="userShowImg" />
+                        <div className="userShowTitle">
+                          <span className="userShowUsername">Anna Manny</span>
+                          <span className="userShowTopTitle">Software engineer</span>
+                         </div>
+                    </div>
+                    <div className="userShowBottom">
+
+                    </div>
+                </div>
+                <div className="userUpdate">Usder Update</div>
             </div>
         </div>
     )
