c7da09497ceca2cc254ab956348257418c6125f5
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-12-01 22:33:50 -0800

    Initial commit

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -18,10 +18,12 @@ export default function sidebar(){
                         Dashboard
                     </h3>
                     <ul className="sidebarList">
+                        <Link to="/" className="link">
                         <li className="sidebarListItem active">
                             <LineStyle className="sidebarIcon"/>
                             Home
                         </li>
+                        </Link>
 
                         <li className="sidebarListItem">
                             <Timeline className="sidebarIcon"/>
