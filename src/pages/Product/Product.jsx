52614bdf78e24f30095ff72fbf17424750486b92
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-12-01 20:53:01 -0800

    Initial commit

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Product.css';
+import productInfoImg1 from '../../img/productInfoImg1.png';
 import { Link } from 'react-router-dom';
 import Chart from '../../components/chart/chart';
 import { ProductData } from '../../dummyData';
@@ -17,13 +18,33 @@ export default function Product(){
       <div className="productTop">
         <div className="productTopLeft">
          <Chart data={ProductData} datakey="Sales" title="Sales performance" grid/>
-        </div>
+         </div>
+
         <div className="productTopRight">
           <div className="productInfoTop">
-
+           <im src={productInfoImg1} alt="" className="productInfoImg" />
+           <span className="productName">Apple Airpod</span>
           </div>
           <div className="productInfoBottom">
-            
+           <div className="productInfoItem">
+             <span className="productInfoKey">id:</span>
+             <span className="productInfoValue">123</span>
+           </div>
+
+           <div className="productInfoItem">
+             <span className="productInfoKey">sales:</span>
+             <span className="productInfoValue">5123</span>
+           </div>
+
+           <div className="productInfoItem">
+             <span className="productInfoKey">active:</span>
+             <span className="productInfoValue">yes</span>
+           </div>
+
+           <div className="productInfoItem">
+             <span className="productInfoKey">in stock</span>
+             <span className="productInfoValue">no</span>
+           </div>
           </div>
         </div>
       </div>
